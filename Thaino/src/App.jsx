d953e34993cc0e84886dad0750ca40865c4f74da
Adam Tawfik <Adamttawfik@gmail.com>
2024-11-29 17:11:30 -0500

    Clensing

--- Thaino/src/App.jsx
+++ Thaino/src/App.jsx
@@ -1,30 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Header from './Header.jsx';
-import Home from './Home.jsx';
-import Summarize from './Summarize.jsx';
-import About from './About.jsx';
-import './App.css';
-
-function App() {
-  return (
-    <Router>
-      <div className="background">
+import Record from './Sound.jsx';
+function App(){
+  return ( 
+    <>       
         <Header />
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/summarize">Summarize</Link></li>
-            <li><Link to="/about">About</Link></li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/summarize" component={Summarize} />
-          <Route path="/about" component={About} />
-        </Switch>
-      </div>
-    </Router>
+        <Record />
+        
+    </>
   );
 }
 
--- Thaino/src/Home.jsx
+++ Thaino/src/Home.jsx
@@ -1,12 +1,26 @@
 import Header from './Header.jsx';
-import Record from './Sound.jsx';
-import Data from './data.jsx'
-
+import './Home.css';
 function Home(){
-  return ( 
-    <> 
-        <Header />
-    </>
+  return (
+    <div class="main">
+    <div class="container">
+      <script type="module" src="/src/main.jsx"></script>
+    </div>
+    <div class="content">
+      <img class="pic" src="public/brain.png" alt="image of brain" />
+      <h4 class="h2">Thaino is an AI software that specializes in helping students become better learners and students. Thaino aims to better student grades and help students to have a better experience in high school and beyond.</h4>
+    </div>
+    
+    <div class="reverse-content">
+      <div class="picture">
+        <img class="pic2" src="public/fun.png" alt="A girl having fun" />
+      </div>
+      <div class="s">
+        <h4 class="para2">Thaino aims to improve student attention spans as well as make school more interesting.</h4>
+      </div>
+    </div>
+    <Header />
+  </div>
   );
 }
 
