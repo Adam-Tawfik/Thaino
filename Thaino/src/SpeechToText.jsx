d2e17538f71e9312794497c073b7f96ceb60780f
Adam Tawfik <Adamttawfik@gmail.com>
2024-11-29 11:25:06 -0500

    Speech to text ready

---
+++ Thaino/src/SpeechToText.jsx
@@ -0,0 +1,28 @@
+import fs from "fs";
+import OpenAI from "openai";
+
+
+
+
+async function main() {
+  const transcription = await openai.audio.transcriptions.create({
+    file: fs.createReadStream("/path/to/file/audio.mp3"),
+    model: "whisper-1",
+  });
+
+  return transcription;
+}
+
+function SpeechToText(){
+    
+    const text = main();
+    return (
+        <div>
+            <p>
+                {text}
+            </p>
+        </div>
+    );
+}
+
+export default SpeechToText;
